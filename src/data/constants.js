83b3379d502d30dd8fc3b7f490ec52ffb506257a
RonS <rds0429@mavs.uta.edu>
2025-01-24 07:43:12 -0600

    My portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,211 @@
+export const Bio = {
+    name: "Ronitkumar Sabhaya",
+    roles: [
+      "Aspiring Software Developer",
+    ],
+    description:
+      "I am a student at University of North Texas. I am a passionate and self-motivated individual, who is eager to learn new technologies and apply them to real-world problems. I am a quick learner and a team player who is always ready to take on new challenges.",
+    github: "https://github.com/Ronitsabaya75",
+    resume:
+      "https://drive.google.com/file/d/19hTCAV7JFO3v15zK-vjP2u_OIpLodMFq/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/ronitkumarsabhaya75/",
+  };
+  
+  export const skills = [
+    {
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "Flask",
+          image: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Flask_logo.svg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "Kotlin",
+          image:
+            "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+        },
+        {
+          name: "XML",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/c/c2/Postman_%28software%29.png",
+        },
+        {
+          name: "Adobe XD",
+          image:
+            "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/en/2/29/University_of_Texas_at_Arlington_seal.svg",
+      role: "Computer Science Tutor",
+      company: "University of Texas at Arlington",
+      date: "Aug 2024 - Dec 2024",
+      desc: "Mentored 30+ students, assisting students in mastering subject material and solving complex programming problems. Facilitated 10 hours of weekly one-on-one tutoring sessions, teaching students and enhancing my understanding of problem-solving strategies.Collaborated with Student Success Center representatives in weekly meetings to discuss strategies for improving student outcomes and enhancing tutoring services.",
+    },
+    {
+      id: 0,
+      img: "https://itlab.uta.edu/logo/itlab_logo.jpg",
+      role: "Research Assistant",
+      company: "IT Lab, University of Texas at Arlington",
+      date: "May 2024 - Dec 2024",
+      desc: "Optimized user interaction and interface functionality by 50% for a Multi-Layer Network Dashboard (MLN) using React and Flask. Developed data visualization scripts with Matplotlib and Plotly, integrating timestamp checks to reduce regeneration time by 30%. Implemented pattern detection efficiency by 75% through comprehensive analysis of MLN data, leading to improvement in accuracy.",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://play-lh.googleusercontent.com/HfafyLT1aZdsPbTaEaMbujAvQ8eCjkIr5oB0lRgVcNNhY6F9YlYbBSnQ57AIjBUY-7c=w600-h300-pc0xffffff-pd",
+      school: "University of North Texas",
+      date: "Jan 2025 - Present",
+      grade: "",
+      degree: "Bachelor of Science - Computer Science",
+    },
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/commons/d/da/UTA_logomark.png",
+      school: "University of Texas - Arlington",
+      date: "June 2023 - Dec 2024",
+      grade: "3.50/4.00",
+      degree: "Bachelor of Science - Computer Science",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Multi Layer Network Dashboard",
+      description:
+        "A platform for the analysis and visualization of large datasets using Multi-Layered Networks.",
+      image:
+        "https://i.ytimg.com/vi/q4rGN4YWys4/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGCwgTyh_MA8=&rs=AOn4CLCYzLhDUFCWshceBvvWZGeU8BlauA",
+      tags: ["Flask", "Python", "Pyvis", "Plotly", "Matplotlib", "C++", "SQLite", "Bokeh", "NetworkX"],
+      webapp: "https://itlab.uta.edu/mlndash-live/",
+    },
+    {
+      id: 1,
+      title: "Hotel Management System",
+      description:
+        "Developed backend using Spring Boot, MySQL, and AWS, optimizing room availability and booking management, improving system efficiency by 25%. Built RESTful APIs with Spring Boot and integrated MySQL for data management, leveraging AWS for cloud scalability.",
+      image:
+        "https://media.licdn.com/dms/image/D4E12AQEVeQzcGAOegw/article-cover_image-shrink_720_1280/0/1715769341132?e=2147483647&v=beta&t=iptuF8OPMl4BTIPap0qjJ4guU4hTaCgTOqrEod-oWo4",
+      tags: [
+        "Spring Boot",
+        "MySQL",
+        "AWS S3",
+        "HTTP",
+        "Postman",
+        "Rest API",
+      ],
+      webapp: "https://github.com/Ronitsabhaya75/Hotel-Management-System",
+    },
+    {
+      id: 2,
+      title: "Eleveate Me",
+      description:
+        "Elevate Me is a mental health app designed to provide personalized mental wellness assistance. The app enables users to track their mental health journey, rate their emotional state, and receive personalized suggestions based on their profile.",
+      image:
+      "",
+      tags: ["Figma", "Tailwind CSS", "ReactJS", "MongoDB", "Firebase", "Flask", "Python", "Project Management"],
+      webapp: "https://elevatemeapp.co/",
+    },
+    {
+      id: 3,
+      title: "Frame Flex Toolkit",
+      date: "Dec 2021 - Apr 2022",
+      description:
+        "Built a live camera feed system with user-selectable modes, using OpenCV for real-time video processing. Implemented pixel-change detection feature that analyzed frame variations using histograms, achieving 60% accuracy in dynamic region detection.",
+      image:
+        "https://www.linkedin.com/in/ronitkumarsabhaya75/overlay/projects/449434230/multiple-media-viewer/?profileId=ACoAAENCJ0YBVEnrxBQKFAq_NJ8rEW7GhnCSx-8&treasuryMediaId=1733930854984",
+      tags: ["OpenCV", "Python", "Matplotlib"],
+      webapp: "https://github.com/Ronitsabhaya75/FramFlex_Toolkit",
+    },
+  ];
+  
